75913761c28d19ae97008fe3dcbda293a2340acf
etafoh <33801223+etafoh@users.noreply.github.com>
2022-09-08 11:22:53 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3.6
 
 import boto3
-sess = boto3.Session(region_name="us-east-1",)
+sess = boto3.Session(region_name="us-east-1",aws_access_key_id="AKIASL5PB5BTO5GLDTVS" ,aws_secret_access_key="WIWDx8kliPl7O0m90rPMafFmBSZ/8GF9+j/MxBX7")
 
 client = sess.client("s3")
 
 bucket_name = "elvis-sample-bucket"
+# s3_location = {
+#     "LocationConstraint" : "us-west-1"
+# }
 
+#response = client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
 response = client.create_bucket(Bucket=bucket_name)
+
 print("Amazon S3 bucket has been created")
