e53790636abcb363e570cd2ff930b7497a328ab6
etafoh <33801223+etafoh@users.noreply.github.com>
2022-09-08 10:17:17 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print('Hello World')
+#!/usr/bin/env python3.6
+
+import boto3
+sess = boto3.Session(region_name="us-east-1",)
+
+client = sess.client("s3")
+
+bucket_name = "elvis-sample-bucket"
+
+response = client.create_bucket(Bucket=bucket_name)
+print("Amazon S3 bucket has been created")
